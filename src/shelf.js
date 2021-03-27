ffda6c90eb523ada365231718b643f3c27d7f6dc
Erica <>
2021-03-27 14:33:16 -0600

    Add Shelf files

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,37 @@
+function shelfBook(bookTitle, shelf) {
+  if (shelf.length < 3) {
+return shelf.unshift(bookTitle);
+}
+};
+
+function unshelfBook(title, shelf) {
+  for(var i = 0; i < shelf.length; i++) {
+    if(title === shelf[i].title) {
+            var titleIndex = shelf.indexOf(shelf[i])
+     shelf.splice(titleIndex, 1)
+   }
+  }
+}
+
+function listTitles(shelf) {
+  var titles = [];
+  for(var i = 0; i < shelf.length; i++) {
+    titles.push(shelf[i].title);
+  }
+    return titles.join(", ");
+}
+
+ function searchShelf(shelf, isTrue) {
+   var titles = [];
+   for(var i = 0; i < shelf.length; i++) {
+   titles.push(shelf[i].title);
+  }
+  return titles.includes(isTrue);
+ }
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  shelfBook,
+  unshelfBook,
+  listTitles,
+  searchShelf
 };
