e73f7298452375fb5cb35c459ef823a15ff35078
Erica <>
2021-03-28 08:32:24 -0600

    Add Library files

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,33 @@
+function createLibrary(name) {
+var library = {
+  name: name,
+  shelves: {
+    fantasy: [],
+    fiction: [],
+    nonFiction: [],
+  },
+}
+return library;
+};
 
+function addBook(library, book) {
+library.shelves[book.genre].push(book);
+};
+
+
+function checkoutBook(library, bookTitle, genre) {
+for (var i = 0; i < library.shelves[genre].length; i++) {
+ var bookName = library.shelves[genre][i].title;
+ if (bookTitle === bookName) {
+  library.shelves[genre].splice([i], 1);
+  return (`You have now checked out ${bookName} from the ${library.name}`);
+}
+}
+return (`Sorry, there are currently no copies of ${bookTitle} available at the ${library.name}`);
+};
 
 module.exports = {
-  // createLibrary,
-  // addBook,
-  // checkoutBook
+  createLibrary,
+  addBook,
+  checkoutBook
 };
